670dab2fe105ea10fcb0f6c7e47fb7217f7f7f0f
Lani555 <alena@alenasazonova.com>
2026-06-12 10:42:11 -0700

    Update checkout.js

--- netlify/functions/checkout.js
+++ netlify/functions/checkout.js
@@ -1,36 +1,30 @@
 exports.handler = async (event) => {
-
   const params = event.queryStringParameters || {};
   const productsParam = params.products || '';
 
-  // If no products passed, just go to the shop
+  const productMap = {
+    '675c7dada75d8b573bd9ef72': 'alpha',
+    '675c80a984cc816fc3335b84': 'beta',
+    '675c810fa5c9013d84ff4a9d': 'theta',
+    '68855d0e41a7fe0160320ed0': 'sample',
+  };
+
   if (!productsParam) {
     return {
       statusCode: 302,
-      headers: {
-        Location: 'https://portalcandle.com/shop',
-        'Cache-Control': 'no-cache',
-      },
+      headers: { Location: 'https://portalcandle.com/shop', 'Cache-Control': 'no-cache' },
     };
   }
 
-  // Meta sends: products=PRODUCT_ID:QUANTITY,PRODUCT_ID2:QUANTITY2
-  // Example:    products=675c810fa5c9013d84ff4a9d:1,675c80a984cc816fc3335b84:2
-  const productEntries = productsParam.split(',');
-
-  // Take the first product (Squarespace addProductId supports one at a time)
-  const firstEntry = productEntries[0];
-  const [productId, quantity] = firstEntry.split(':');
-
-  // Build the Squarespace cart URL
-  const redirectUrl = `https://portalcandle.com/cart?addProductId=${encodeURIComponent(productId)}&quantity=${encodeURIComponent(quantity || '1')}`;
+  const firstEntry = productsParam.split(',')[0];
+  const [productId] = firstEntry.split(':');
+  const slug = productMap[productId];
 
   return {
     statusCode: 302,
     headers: {
-      Location: redirectUrl,
+      Location: slug ? `https://portalcandle.com/shop/p/${slug}` : 'https://portalcandle.com/shop',
       'Cache-Control': 'no-cache',
     },
   };
-
 };
