a3f04f81829dbf88fbeacbaa2f7e6179573f1d72
Lani555 <alena@alenasazonova.com>
2026-06-12 10:58:16 -0700

    Update checkout.js

--- netlify/functions/checkout.js
+++ netlify/functions/checkout.js
@@ -2,13 +2,6 @@ exports.handler = async (event) => {
   const params = event.queryStringParameters || {};
   const productsParam = params.products || '';
 
-  const productMap = {
-    '675c7dada75d8b573bd9ef72': 'alpha',
-    '675c80a984cc816fc3335b84': 'beta',
-    '675c810fa5c9013d84ff4a9d': 'theta',
-    '68855d0e41a7fe0160320ed0': 'sample',
-  };
-
   if (!productsParam) {
     return {
       statusCode: 302,
@@ -16,14 +9,10 @@ exports.handler = async (event) => {
     };
   }
 
-  const firstEntry = productsParam.split(',')[0];
-  const [productId] = firstEntry.split(':');
-  const slug = productMap[productId];
-
   return {
     statusCode: 302,
     headers: {
-      Location: slug ? `https://portalcandle.com/shop/p/${slug}` : 'https://portalcandle.com/shop',
+      Location: `https://portalcandle.com/meta-checkout?products=${encodeURIComponent(productsParam)}`,
       'Cache-Control': 'no-cache',
     },
   };
